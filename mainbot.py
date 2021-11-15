6c1facf49f3b4ccfdc0e4cebfd2b58959e9e9d5d
ammoriddin <94297206+ammoriddin@users.noreply.github.com>
2021-11-15 19:58:41 +0500

    initial commit

---
+++ mainbot.py
@@ -0,0 +1,35 @@
+import requests
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '2118595678:AAH_Xm5C0mkrOT2iHiGuFsYrf9fn3BjePPY'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def salom(message: types.Message):
+    await message.reply("Salom botimizga xush kelibsiz!!")
+
+@dp.message_handler(commands=['help'])
+async def yordam(message: types.Message):
+    await message.reply("botimiz sizga 1dollir necha so'm ligini aytib beradi")
+
+
+
+@dp.message_handler(commands=['dollar'])
+async def kurslar(message: types.Message):
+    API_KEY = 'cc5bf5435756bb4eea3c0f09'
+    curresy = 'USD'
+    url = f'https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{curresy}/UZS'
+    r = requests.get(url)
+    kurs = r.json()['conversion_rate']
+    print(f'bugungi kurs: 1AQSH dollar = {kurs} so\'m')
+
+    await message.answer(f'bugungi kurs: 1AQSH dollir= {(kurs)} so\'m')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
